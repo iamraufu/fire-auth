a6eb87b4c097c434a31a353a1737c80888b2aeaa
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-17 12:32:34 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-  const [user,setUser] =useState({
+  const [user, setUser] = useState({
     isSignedIn: false,
     name: '',
     email: '',
@@ -16,53 +16,69 @@ function App() {
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
-    .then(result => {
-      const {displayName,photoURL, email} = result.user;
-      const signedInUser = {
-        isSignedIn: true,
-        name: displayName,
-        email: email,
-        photo:photoURL
-      }
-      setUser(signedInUser);
-      console.log(displayName,photoURL,email)
-    })
-    .catch(err=>{
-      console.log(err);
-      console.log(err.message);
-    })
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+        console.log(displayName, photoURL, email)
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
   }
 
-  const handleSignOut=()=>{
+  const handleSignOut = () => {
     firebase.auth().signOut()
-    .then(result => {
-      const signedOutUser ={
-        isSignedIn: false,
-        name: '',
-        email: '',
-        photo:''
-      }
-      setUser(signedOutUser);
-    }).catch((error) => {
-      console.log(error)
-    });
+      .then(result => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: ''
+        }
+        setUser(signedOutUser);
+      }).catch((error) => {
+        console.log(error)
+      });
+  }
+
+  const handleBlur = (event) => {
+    console.log(event.target.name, event.target.value)
+  }
+
+  const handleSubmit = () => {
+    console.log("submitted")
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        { user.isSignedIn ? 
+        {user.isSignedIn ?
           <button onClick={handleSignOut}>Sign Out</button> :
           <button onClick={handleSignIn}>Sign In</button>
         }
         {
-          user.isSignedIn && 
+          user.isSignedIn &&
           <div className="">
             <p>Welcome, {user.name}</p>
-          <p>Your email: {user.email}</p>
-          <img src={user.photo} alt=''/>
+            <p>Your email: {user.email}</p>
+            <img src={user.photo} alt='' />
           </div>
         }
+        <h1>Authentication System</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onBlur={handleBlur} name='email' placeholder='Your Email' required />
+          <br />
+          <input type="password" onBlur={handleBlur} name="password" id="" placeholder='Your Password' required />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
